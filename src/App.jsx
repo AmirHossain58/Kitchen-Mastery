50dbf8d4a7b518abaaa6cadd1f466062950c1f94
AmirHossain58 <ahantar58@gmail.com>
2024-03-15 22:24:30 +0600

    project setup with tailwind css and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header"
+
+
+function App() {
+
+
+  return (
+    <div className="mt-12">
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
